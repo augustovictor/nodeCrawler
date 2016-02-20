77ae48dcd6ee9e05dc94ec34cb3706bb12d02103
Victor <victoraweb@gmail.com>
2016-02-20 18:32:13 -0300

    Finds specific word in given webpage>subpages

--- crawler.js
+++ crawler.js
@@ -1,41 +1,83 @@
 var request = require('request');
-
 // Parse and select HTML elements on the page
 var cheerio = require('cheerio');
-
 // Parse URLs
 var URL = require('url-parse');
 
-// Crawler
-var pageToVisit = "http://www.anp.gov.br/preco/prc/Resumo_Por_Estado_Index.asp";
+var START_URL = 'http://www.arstechnica.com';
+var SEARCH_WORD = 'stemming';
+var MAX_PAGES_TO_VISIT = 10;
 
-console.log('Visiting page' + pageToVisit);
+var pagesVisited = {};
+var numPagesVisited = 0;
+var pagesToVisit = [];
+var url = new URL(START_URL);
+var baseUrl = url.protocol + '//' + url.hostname;
 
-request(pageToVisit, function (error, response, body) {
-    'use strict';
+pagesToVisit.push(START_URL);
+crawl();
 
-    if (error) {
-        console.log('Error ' + error);
-    }
+///////////////////////////////////////////////////////
 
-    // Check status code; (200 is HTTP OK)
-    console.log('Status code: ' + response.statusCode);
+function crawl() {
+  if(numPagesVisited >= MAX_PAGES_TO_VISIT) {
+    console.log("Reached max limit of number of pages to visit.");
+    return;
+  }
+  var nextPage = pagesToVisit.pop();
+  if (nextPage in pagesVisited) {
+    // We've already visited this page, so repeat the crawl
+    crawl();
+  } else {
+    // New page we haven't visited
+    visitPage(nextPage, crawl);
+  }
+}
 
-    if (response.statusCode === 200) {
+function visitPage(url, callback) {
+  // Add page to our set
+  pagesVisited[url] = true;
+  numPagesVisited++;
 
-        // Parse document body
-        var $ = cheerio.load(body);
+  // Make the request
+  console.log("Visiting page " + url);
+  request(url, function(error, response, body) {
+     // Check status code (200 is HTTP OK)
+     console.log("Status code: " + response.statusCode);
+     if(response.statusCode !== 200) {
+       callback();
+       return;
+     }
+     // Parse the document body
+     var $ = cheerio.load(body);
+     var isWordFound = searchForWord($, SEARCH_WORD);
+     if(isWordFound) {
+       console.log('Word \'' + SEARCH_WORD + '\' found at page ' + url);
+     } else {
+       collectInternalLinks($);
+       // In this short program, our callback is just calling crawl()
+       callback();
+     }
+  });
+}
 
-        var captchaCode = '';
+// Looking for a specific word
+function searchForWord($, word) {
+    var bodyText = $('html > body').text().toLowerCase();
 
-        for (var i = 1; i <= 4; i++) {
-            captchaCode += $('label#letra' + i).text();
-        }
-        
-//        captchaCode.join('');
+    return (bodyText.indexOf(word.toLowerCase()) !== -1);
+}
 
-        console.log('Captcha code: ' + captchaCode);
+// Collect internal links
+function collectInternalLinks($) {
 
-    }
+    var relativeLinks = $("a[href^='/']");
 
-});
+    console.log('Found ' + relativeLinks.length + ' relative links.');
+
+    relativeLinks.each(function () {
+        pagesToVisit.push(baseUrl + $(this).attr('href'));
+    });
+
+
+}
